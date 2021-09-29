c46cbf25eab5032bf9f479f5c0246fbfe27c022e
patrick-lee22 <prlee@wpi.edu>
2021-09-29 19:39:16 -0400

    Copying over a3 into this repo

---
+++ README.markdown
@@ -0,0 +1,52 @@
+## Better OwOifier
+
+For this project, I used Heroku for deployment. Here is the link to my deployed project: https://a3-patrick-lee22.herokuapp.com/
+
+The purpose of this application is simple: it's a very rudimentary message board. A user logs in with credentials that they create, and can create messages to any other possible user (even ones with usernames that haven't been claimed yet).
+Upon logging into the app, a user will be shown all messages sent to them by any user (even God). They can view the messages in two different views:
+
+1. Table view. This shows the message as a table with two columns: the sender and the message, along with the owo-ified versions of those two fields.
+2. Sentence view. This renders the message as a sentence, both normal and owo-ified.
+
+Users can also send messages to anyone they like. The user can input the name of the user to send the message to and the message, along with two key visual options:
+
+1. A fancy text option. This changes the font of the message in all views from the default font to a cursive, swirly font. This option is for those with a flair for the dramatic.
+2. A selection of options to choose what replaces exclaimation points in the text of the message. There are three options: text faces (this is the default), emojis, and no replacement.
+
+After sending a message, users can edit their original message in the following ways:
+
+1. Users can change the contents of the message.
+2. Users can change the font fanciness.
+3. Users can change the '!' replacement option.
+
+It is worth noting that users CANNOT change who the message was sent to. However, if a message was sent in error, the user may instead delete the message and send a new message to the intended recipient.
+
+Authentication in this application is very simple. To create an account, all a new user has to do is input a username and password in the login fields when first entering the application. If the username is taken, there will be a short error message displayed.
+This strategy was chosen both due to its ease of implementation and its ease of use.
+
+Much of the visual style of this application comes from the Bootstrap framework, which was used during to give this app a clean look and feel. I chose bootstrap for this application partly due to familiarity with the framework from prior work experience, but also because of the ease of creating interactive animations and clean-looking visuals. Examples of out-of-the-box bootstrap visual effects that enhance this application include:
+
+- Hovering labels for input fields
+- Table stylings
+- Font choice (non-fancy, of course)
+- Button stylings, including hover-fill animations
+- Navbar stylings
+
+Another reason why bootstrap was my framework of choice is its grid system. Using css classes, I was able to align items within the application simply by adding a few classes and wrapping div elements here and there. For examples of this, look to the sent messages view.
+
+There were very few css rules I wrote for this project, and the ones which I did write were largely things like borders and extra padding.
+
+Five Express middleware packages were used in creating this application. Here is a list of them, along with what they do.
+
+1. serve-static: this middleware package serves static files (like the css and javascript files powering the application).
+2. cookie-session: used to create a cookie to store the username of the currently logged in user.
+3. morgan: this is used to log http request information to the console, for ease of developing the app and monitoring it (plus I like things with my name).
+4. serve-favicon: this is used to easily send the favicon I created for this application.
+5. body-parser: this is used to automatically parse json data from the client.
+
+## Technical Achievements
+- **Tech Achievement 1**: As mentioned previously, I used Heroku as my hosting platform for this assignment. While it had a slightly steeper learning curve than Glitch, there was a distinct advantage to using Heroku: automatic redeployment upon a new commit to the base repository. When using Glitch, if I updated the code base locally and then committed my changes, I had to create a completely new project on Glitch to see those changes reflected in the code. Heroku will automatically redeploy the project whenever a new commit is pushed to the origin.
+- **Tech Achievement 2**: My application scored 100% on all four lighthouse tests, on both the login page and the main message board. I have included screenshots as proof.
+![report-login](https://user-images.githubusercontent.com/65562105/134834946-1b381647-0e65-4510-9cbb-17306f7256e3.PNG)
+![report-messageboard](https://user-images.githubusercontent.com/65562105/134834953-79c5efc7-6764-4fae-b148-f45e4301aff7.PNG)
+
---
+++ public/js/message_board.js
@@ -0,0 +1,273 @@
+const submit = function (e) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const nameText = document.getElementById('name');
+    const targetText = document.getElementById('target-text');
+    const fancyFont = document.getElementById('fancy-font');
+    const textFaces = document.getElementById('text-faces');
+    const emojis = document.getElementById('emojis');
+    let checkedRadio = -1;
+    if (textFaces.checked) {
+        checkedRadio = 0;
+    } else if (emojis.checked) {
+        checkedRadio = 1;
+    } else {
+        checkedRadio = 2;
+    }
+    json = {
+        name: nameText.value,
+        message: targetText.value,
+        fancyFont: fancyFont.checked,
+        checkedRadio: checkedRadio
+    };
+    body = JSON.stringify(json)
+
+    if (nameText.value !== '' && targetText.value !== '') {
+        fetch('/submit', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body
+        })
+            .then(function (response) {
+                // do something with the reponse 
+                response.text().then(function (str) {
+                    let jsonObj = JSON.parse(str);
+                    if (jsonObj.loggedInUser === jsonObj.obj.to) {
+                        addNewElt(jsonObj.obj);
+                    }
+                    addSentElement(jsonObj.obj);
+                })
+            })
+
+    } else {
+        alert("Please specify a user to receive a message, as well as the message itself.")
+    }
+    return false
+}
+
+const switchView = function (e) {
+    document.getElementsByClassName('active')[0].classList.remove('active');
+    e.target.classList.add('active');
+    if (e.target.id === 'table-nav') {
+        document.getElementById('table-root').style.display = 'flex';
+        document.getElementById('sentence-root').style.display = 'none';
+        document.getElementById('sent-root').style.display = 'none';
+    } else if (e.target.id === 'sentence-nav') {
+        document.getElementById('table-root').style.display = 'none';
+        document.getElementById('sentence-root').style.display = 'flex';
+        document.getElementById('sent-root').style.display = 'none';
+    } else {
+        document.getElementById('table-root').style.display = 'none';
+        document.getElementById('sentence-root').style.display = 'none';
+        document.getElementById('sent-root').style.display = 'flex';
+    }
+}
+
+function addNewElt(element) {
+    let normalTable = document.getElementById('normal-table');
+    let owoTable = document.getElementById('owo-table');
+    let normalSentenceRoot = document.getElementById('normal-sentence-root');
+    let owoSentenceRoot = document.getElementById('owo-sentence-root');
+
+    let normalrow = normalTable.insertRow(-1);
+    normalrow.insertCell(0).innerHTML = element.from;
+    let messagecell = normalrow.insertCell(1);
+    messagecell.innerHTML = element.message;
+    let oworow = owoTable.insertRow(-1);
+    oworow.insertCell(0).innerHTML = element.fromowo;
+    let messageowocell = oworow.insertCell(1);
+    messageowocell.innerHTML = element.messageowo;
+    let normalSentence = document.createElement('p');
+    normalSentence.innerHTML = `${element.from} said: "${element.message}"`;
+    let owoSentence = document.createElement('p');
+    owoSentence.innerHTML = `${element.fromowo} said: "${element.messageowo}"`;
+    owoSentence.classList.add('sentence');
+    normalSentence.classList.add('sentence');
+    if (element.fancyFont) {
+        messagecell.className = 'fancy-font';
+        messageowocell.className = 'fancy-font';
+        normalSentence.classList.add('fancy-font');
+        owoSentence.classList.add('fancy-font');
+    }
+    normalSentenceRoot.append(normalSentence);
+    owoSentenceRoot.append(owoSentence);
+}
+
+function addSentElement(element, idx) {
+    const editRoot = document.getElementById('editable-root');
+    let newForm = document.createElement('form');
+    newForm.classList.add('row', 'inline-form');
+    newForm.innerHTML = `
+    <div class='form-floating col-4'>
+        <textarea class='form-control' placeholder='message goes here' name='message' id='message${idx}'>${element.message}</textarea>
+        <label for='message'>Message</label>
+    </div>
+    <div class='form-check col'>
+        <label class='form-check-label' for='fancy-font'>Fancy Font?</label>
+        <input class='form-check-input' type="checkbox" id='fancy-font${idx}'>
+    </div>
+    <div class='col'>
+        <div class='row'>
+            <div class='form-check col'>
+                <label class='form-check-label' for='text-faces${idx}'>Replace '!' With Text Faces</label>
+                <input class='form-check-input' type='radio' name='facereplace${idx}' id='text-faces${idx}'>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='form-check col'>
+                <label class='form-check-label' for='emojis${idx}'>Replace '!' With Emojis</label>
+                <input class='form-check-input' type='radio' name='facereplace${idx}' id='emojis${idx}'>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='form-check col'>
+                <label class='form-check-label' for='no-replace${idx}'>Do Not Replace '!'</label>
+                <input class='form-check-input' type='radio' name='facereplace${idx}' id='no-replace${idx}'>
+            </div>
+        </div>
+    </div>
+    <div class='col'>
+        <div class='row'>
+            <button class='btn btn-outline-primary inline-update-btn' id='inline-update-btn${idx}' data-index=${idx} data-dbid=${element._id}>Update</button>
+        </div>
+        <div class='row'>
+            <button class='btn btn-outline-secondary inline-delete-btn' id='inline-delete-btn${idx}' data-index=${idx} data-dbid=${element._id}>Delete</button>
+        </div>
+    </div>`;
+    editRoot.append(newForm);
+    switch (element.oworeplace) {
+        case 0:
+            document.getElementById('text-faces' + idx).checked = true;
+            break;
+        case 1:
+            document.getElementById('emojis' + idx).checked = true;
+            break;
+        case 2:
+            document.getElementById('no-replace' + idx).checked = true;
+            break;
+    }
+    if (element.fancyFont) {
+        document.getElementById('fancy-font' + idx).checked = true;
+    }
+
+    const updateBtn = document.getElementById('inline-update-btn' + idx);
+    const deleteBtn = document.getElementById('inline-delete-btn' + idx);
+
+    let updateFunc = function (e) {
+        e.preventDefault;
+        let btn = e.srcElement
+        let index = btn.getAttribute('data-index');
+        let dbid = btn.getAttribute('data-dbid');
+        let checkedRadio = -1;
+        if (document.getElementById('text-faces' + index).checked) {
+            checkedRadio = 0;
+        } else if (document.getElementById('emojis' + index).checked) {
+            checkedRadio = 1
+        } else {
+            checkedRadio = 2;
+        }
+        let obj = {
+            dbid: dbid,
+            message: document.getElementById('message' + index).value,
+            fancyFont: document.getElementById('fancy-font' + index).checked,
+            checkedRadio: checkedRadio
+        }
+        let body = JSON.stringify(obj);
+        fetch('/update', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body
+        }).then(function (response) {
+            clearAllMessages();
+            fetchAllMessages();
+        });
+    }
+    updateBtn.onclick = updateFunc;
+    let deleteFunc = function (e) {
+        e.preventDefault;
+        let btn = e.srcElement
+        let dbid = btn.getAttribute('data-dbid');
+        let obj = {
+            dbid: dbid
+        }
+        let body = JSON.stringify(obj);
+        fetch('/delete', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body
+        }).then(function (response) {
+            clearAllMessages();
+            fetchAllMessages();
+        });
+    }
+    deleteBtn.onclick = deleteFunc;
+
+}
+
+function clearAllMessages() {
+    const normalTable = document.getElementById('normal-table');
+    const owoTable = document.getElementById('owo-table');
+    const normalSentence = document.getElementById('normal-sentence-root');
+    const owoSentence = document.getElementById('owo-sentence-root');
+    const sentMessages = document.getElementById('editable-root')
+
+    normalTable.innerHTML =
+        `<thead>
+        <tr>
+        <th>Name</th>
+        <th>Message</th>
+        </tr>
+    </thead>`;
+    owoTable.innerHTML =
+        `<thead>
+        <tr>
+        <th>Name</th>
+        <th>Message</th>
+        </tr>
+    </thead>`;
+    normalSentence.innerHTML = '<b>Normal Sentences</b>';
+    owoSentence.innerHTML = '<b>OwO-ified Sentences</b>';
+    sentMessages.innerHTML = '<b>Sent Messages</b>';
+
+}
+
+function fetchAllMessages() {
+    fetch('/getMessagesTo', {
+        method: 'GET',
+    }).then(function (response) {
+        response.text().then(function (jsonData) {
+            console.log(jsonData)
+            let appdata = JSON.parse(jsonData);
+            if (appdata.length !== 0) {
+                appdata.forEach(element => addNewElt(element));
+            }
+        });
+    });
+
+    fetch('/getMessagesFrom', {
+        metod: 'GET',
+    }).then(function (response) {
+        response.text().then(function (jsonData) {
+            console.log(jsonData);
+            let appdata = JSON.parse(jsonData);
+            if (appdata.length !== 0) {
+                appdata.forEach(function (elt, idx) {
+                    addSentElement(elt, idx);
+                });
+            }
+        });
+    })
+}
+
+window.onload = function () {
+    const button = document.getElementById('form-submit-btn')
+    button.onclick = submit;
+    const tableLink = document.getElementById('table-nav');
+    const sentenceLink = document.getElementById('sentence-nav');
+    const sentLink = document.getElementById('sent-nav');
+    tableLink.onclick = switchView;
+    sentenceLink.onclick = switchView;
+    sentLink.onclick = switchView;
+    fetchAllMessages();
+}
