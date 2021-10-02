5b96b1353c57595a42f706259167f2b615a1b27a
vspatel927 <89553998+vspatel927@users.noreply.github.com>
2021-10-02 15:09:40 -0400

    Add, Edit and Delete should work now

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ class App extends React.Component {
               <td>{entry.weight}</td>
               <td>{entry.bmi}</td>
               <td>{entry.status}</td>
-              <td><button id = "edit + {i}"  onClick ={e => this.editEntry(entry, i)} >Edit</button></td>
+              <td><button id = {i}  onClick ={e => this.editEntry(this.state.entries[i], e)} >Edit</button></td>
               <td><button id= {i} onClick={e => this.deleteEntry(this.state.entries[i], e)}>Delete</button></td>
             </tr>
             ) }
@@ -81,31 +81,31 @@ class App extends React.Component {
     )
   }
 // when an entry is toggled, send data to server
-editEntry( entry, index) {
+editEntry( entry, e) {
   const newname = document.getElementById('yourname')
   const newfeet = document.getElementById('feet')
   const newinches = document.getElementById('inches')
   const newweight = document.getElementById('weight')
-  const button = document.getElementById('edit')
+  const button = e.target
 
   newname.value = entry.name
   newfeet.value = entry.feet
   newinches.value = entry.inches
   newweight.value = entry.weight
-  button.innerHTML = 'Save'
+  button.innerText = 'Save'
 
   button.onclick = function(){
-
   fetch( '/change', {
     method:'POST',
-    body: JSON.stringify({index: index, name:newname.value, feet: newfeet.value, inches:newinches.value, weight: newweight.value, bmi:'0', status:'Healthy'}),
+    body: JSON.stringify({index: e.target.id, name:newname.value, feet: newfeet.value, inches:newinches.value, weight: newweight.value, bmi:'0', status:'Healthy'}),
     headers: { 'Content-Type': 'application/json' }
   })
   .then(response => response.json())
   .then(json => {
     this.setState({entries:json})
   })
-  button.innerHTML = 'Edit'
+  .catch(err => console.log(err))
+  button.innerText = 'Edit'
 }
 this.load()
  
