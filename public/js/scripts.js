c5c3c343dd46aa182525171bc8b47a35c937ecb9
AstroSeer <matthewnagy@verizon.net>
2021-10-03 17:51:20 -0400

    imported project a2

---
+++ public/js/scripts.js
@@ -0,0 +1,10 @@
+// Add some Javascript code here, to run on the front end.
+/*
+<table id="tasktable">
+        <tr>
+          <th>Task Name</th>
+          <th>Task Description</th>
+          <th>Due Date</th>
+          <th></th>
+        </tr>
+      </table> */
