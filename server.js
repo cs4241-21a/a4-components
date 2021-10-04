b421b4d8f7dc60c1ccea1c25fd21dca895374f86
Lia Davis <evan4489@me.com>
2021-10-04 11:17:20 -0400

    Ported over 90% of the functionality and used React to generate Message components

--- server.js
+++ server.js
@@ -5,23 +5,56 @@ const todos = [
     { name:'buy groceries', completed:false }
 ]
 
+const appdata = [
+    // { name: 'Jane Doe', color: '#ff55ff', message: 'Sometimes all you need is a little splash of color1' },
+    // { name: 'Jane Doe', color: '#ffff55', message: 'Sometimes all you need is a little splash of color2' },
+    // { name: 'Jane Doe', color: '#55ffff', message: 'Sometimes all you need is a little splash of color3' }
+]
+
 app.use( express.json() )
+
+console.log("Setting up request logger")
+const requestLogs = function(req, res, next){
+    console.log(req.url)
+    next()
+}
+app.use(requestLogs)
 
 // this will most likely be 'build' or 'public'
 app.use( express.static( 'build' ) )
 
-app.get( '/read', ( req, res ) => res.json( todos ) )
 
-app.post( '/add', ( req,res ) => {
-    todos.push( req.body )
-    res.json( todos )
+app.get( '/messages', ( req, res ) => {
+    res.json( appdata )
+} )
+
+app.post( '/submit', (req, res) => {
+    const json = req.body
+    console.log("Received datastring to /submit: " + JSON.stringify(json))
+
+    //Derived field
+    json.message = json.name + " says \"" + json.message + "\""
+
+    appdata.push(json)
+    console.log("Sending: \n" + JSON.stringify(appdata))
+    res.json(appdata)
 })
 
-app.post( '/change', function( req,res ) {
-    const idx = todos.findIndex( v => v.name === req.body.name )
-    todos[ idx ].completed = req.body.completed
 
-    res.sendStatus( 200 )
-})
+// Examples
+// app.get( '/read', ( req, res ) => res.json( todos ) )
+//
+// app.post( '/add', ( req,res ) => {
+//     todos.push( req.body )
+//     res.json( todos )
+// })
+//
+// app.post( '/change', function( req,res ) {
+//     const idx = todos.findIndex( v => v.name === req.body.name )
+//     todos[ idx ].completed = req.body.completed
+//
+//     res.sendStatus( 200 )
+// })
+// End Examples
 
 app.listen( 3003 )
--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {render} from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello, this is cool</p>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       {/*<header className="App-header">*/}
+//       {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+//       {/*  <p>*/}
+//       {/*    Edit <code>src/App.js</code> and save to reload.*/}
+//       {/*  </p>*/}
+//       {/*  <a*/}
+//       {/*    className="App-link"*/}
+//       {/*    href="https://reactjs.org"*/}
+//       {/*    target="_blank"*/}
+//       {/*    rel="noopener noreferrer"*/}
+//       {/*  >*/}
+//       {/*    Learn React*/}
+//       {/*  </a>*/}
+//       {/*  <p>Hello, this is cool</p>*/}
+//       {/*</header>*/}
+//
+//
+//       <header>
+//         <form>
+//           <label>Name:</label>
+//           <input type="text" id="inputName" style ={{width: "100%"}} placeholder="Jane Doe"/>
+//           <br/>
+//           <label>Message Color:</label>
+//           <input type="color" id="inputColor" value="#FF55FF"/>
+//           <br/>
+//           <label>Message:</label>
+//           <br/>
+//           <textarea id="inputMessage" placeholder="Sometimes all you need is a little splash of color"></textarea>
+//           <br/>
+//           <button className="header-btn">Send</button>
+//         </form>
+//       </header>
+//     </div>
+//   );
+// }
+
+class Message extends React.Component {
+  // our .render() method creates a block of HTML using the .jsx format
+  render() {
+    const messageStyle = {
+      backgroundColor: this.props.color
+    };
+    return (
+        <message style={messageStyle}>
+          <h3>{this.props.name}</h3>
+          <p>{this.props.message}</p>
+        </message>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor( props ) {
+    super( props )
+    // initialize our state
+    this.state = { messages:[] }
+    this.load()
+  }
+
+  // load in our data from the server
+  load() {
+    fetch( '/messages', { method:'get', 'no-cors':true })
+        .then( response => response.json() )
+        .then( json => {
+          this.setState({ messages:json })
+        })
+  }
+
+  //send data to the server
+  send(event){
+    const name = document.getElementById( "inputName" ),
+        color = document.getElementById("inputColor"),
+        message = document.getElementById("inputMessage"),
+        json = { name: name.value, color: color.value, message: message.value },
+        body = JSON.stringify( json )
+
+    console.log("Sending the following to the server: " + body)
+
+    fetch( '/submit', {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+  }).then( function( response ) {
+    console.log("Got a response: " + response.status)
+      if (response.ok){
+        //Clear message box
+        document.getElementById("inputMessage").value = ""
+        return response.json()
+      }else {
+        return false
+      }
+    })
+  }
+
+  // render component HTML using JSX
+  render() {
+    return (
+        <div className="App" id="main">
+          <header>
+            <form>
+              <label>Name:</label>
+              <input type="text" id="inputName" style ={{width: "100%"}} placeholder="Jane Doe"/>
+              <br/>
+              <label>Message Color:</label>
+              <input type="color" id="inputColor" value="#FF55FF"/>
+              <br/>
+              <label>Message:</label>
+              <br/>
+              <textarea id="inputMessage" placeholder="Sometimes all you need is a little splash of color"></textarea>
+              <br/>
+              <button className="header-btn" onClick={e => this.send(e)}>Send</button>
+            </form>
+          </header>
+
+          {/*<input type='text' /><button onClick={ e => this.add( e )}>add</button>*/}
+
+          { this.state.messages.map( (message,i) => <Message key={i} name={message.name} color={message.color} message={message.message} /> ) }
+        </div>
+    )
+  }
 }
 
 export default App;
