2f28a96b20fdb3dec7b13ed13f31b8e453f165eb
Glitch (chip-understood-drawer) <none>
2021-10-07 02:33:32 +0000

    ☔️🎓 Checkpoint ./server.js:35537215/426 ./package.json:35537215/1396 ./src/App.svelte:35537215/1838

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,80 @@
 <script>
-	export let name;
+  const getPizzas = function(){
+    const p = fetch('/read', {
+      method: 'GET'
+    })
+    .then( response => response.json())
+    .then( json => {
+      console.log(json)
+      return json
+    })
+    return p
+  }
+  
+  const addPizzaOrder = function( e ){
+    const pizza
+  }
+  
+  let promise = getPizzas
 </script>
+
+{#await promise then pizzas}
+  var pNoGf = pizza.totP - pizza.gfP; //people that aren't gf
+  var totalSl = pizza.slicesPer * pNoGf; //total slices needed
+  var totalGf = pizza.slicesPer * pizza.gfP; //total slices for gf
+  var large = 0;
+  var medium = 0;
+  var small = 0;
+  var largeGf = 0;
+  var mediumGf = 0;
+  var smallGf = 0;
+  while (totalSl > 6) {
+        if (totalSl - 10 >= 0) {
+          large++;
+          totalSl -= 10;
+        } else if (totalSl - 8 >= 0) {
+          medium++;
+          totalSl -= 8;
+        } else {
+          small++;
+          totalSl -= 6;
+        }
+      }
+
+      if (totalSl > 0 && totalSl <= 6) {
+        small++;
+      }
+
+      while (totalGf > 6) {
+        if (totalGf - 10 >= 0) {
+          largeGf++;
+          totalGf -= 10;
+        } else if (totalGf - 8 >= 0) {
+          mediumGf++;
+          totalGf -= 8;
+        } else {
+          smallGf++;
+          totalGf -= 6;
+        }
+      }
+
+      if (totalGf > 0 && totalGf <= 6) {
+        small++;
+      }
+  <table>
+  {#each pizzas as pizza}
+    <td>{pizza.totP} : <input type='text' totP={pizza.totP}></td>
+    <td>{pizza.slicesPer} : <input type='text' slicesPer={pizza.slicesPer}></td>
+    <td>{pizza.gfP} : <input type='text' gfP={pizza.gfP}></td>
+    <td>{large} : large</td>
+    <td>{medium} : medium</td>
+    <td>{small} : small</td>
+    <td>{largeGf} : largeGf</td>
+    <td>{mediumGf} : mediumGf</td>
+    <td>{smallGf} : smallGf</td>
+  {/each}
+  </table>
+{/awat}
 
 <main>
 	<h1>Hello {name}!</h1>
