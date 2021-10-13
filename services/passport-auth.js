994f412d2c099a60d1ce3ae30b96d40a30be82f9
fedeit <fgalbiati@wpi.edu>
2021-10-13 02:01:16 -0400

    Improved auth function

--- services/passport-auth.js
+++ services/passport-auth.js
@@ -1,8 +1,10 @@
 module.exports = (req, res, next) => {
-    if (req.user && req.user.emails && req.user.emails[0]) {
-      next()
-    } else {
-      res.writeHeader( 403, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ 'error': 'User not authenticatd' , 'status': 403}))
-    }
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+  console.log(`User ${ip} is logged in: ${req.isAuthenticated()}`)
+  if (req.isAuthenticated()) {
+      console.log(`User ${ip}: username: ${req.user.username}`)
+      return next();
+  }
+  res.writeHeader( 403, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify({ 'error': 'User not authenticatd' , 'status': 403}))
 }
